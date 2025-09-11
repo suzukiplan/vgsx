1f080bb16a8d7771285c574cd5d4ed4974b8160e
suzukiplan <suzukiplan.ys@gmail.com>
2025-09-11 09:17:13 +0900

    add example project

---
+++ example/01_hello/program.c
@@ -0,0 +1,13 @@
+#include <vgs.h>
+
+void print(const char* text)
+{
+    return;
+}
+
+int main(int argc, char* argv[])
+{
+    const char* text = "HELLO,WORLD!";
+    print(text);
+    return 0;
+}
