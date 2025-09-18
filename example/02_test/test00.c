bef8e867391905861f51acd9afb03466abb5a8a6
suzukiplan <suzukiplan.ys@gmail.com>
2025-09-18 22:14:19 +0900

    use stdarg.h

--- example/02_test/test00.c
+++ example/02_test/test00.c
@@ -1,18 +1,37 @@
 #include <vgs.h>
+#include <stdarg.h>
 
-void log(const char* text, int32_t num)
+void putlog(const char* format, ...)
 {
     char d32[12];
-    vgs_console_print(text);
-    vgs_d32str(d32, num);
-    vgs_console_println(d32);
+    va_list arg;
+    va_start(arg, format);
+    while (*format) {
+        if (format[0] == '%' && format[1] == 'd') {
+            vgs_d32str(d32, va_arg(arg, int32_t));
+            vgs_console_print(d32);
+            format += 2;
+        } else if (format[0] == '%' && format[1] == 'u') {
+            vgs_u32str(d32, va_arg(arg, uint32_t));
+            vgs_console_print(d32);
+            format += 2;
+        } else if (format[0] == '%' && format[1] == 's') {
+            vgs_console_print(va_arg(arg, const char*));
+            format += 2;
+        } else {
+            VGS_OUT_CONSOLE = *format;
+            format++;
+        }
+    }
+    VGS_OUT_CONSOLE = '\n';
+    va_end(arg);
 }
 
 int main(int argc, char* argv)
 {
     int32_t ret = 123;
-    log("call vgs_exit with exit code: ", ret);
+    putlog("call vgs_exit with exit code: %d (%s)", ret, "FOO");
     vgs_exit(ret);
-    vgs_console_println("return 456 (this message will not shown)");
+    putlog("return 456 (this message will not shown)");
     return 456;
 }
