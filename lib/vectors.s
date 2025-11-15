ced5e154cfb71bcff0d07c20b279c166fb47a390
suzukiplan <suzukiplan.ys@gmail.com>
2025-11-16 07:21:46 +0900

    correct vector table

--- lib/vectors.s
+++ lib/vectors.s
@@ -1,11 +1,21 @@
 .section .vectors,"ax"
 .global __vgs_vectors
+
+.macro VGS_VECTOR handler, count=1
+.rept \count
+.long \handler
+.endr
+.endm
+
 __vgs_vectors:
-.long 0xF0FFFFFC
-.long crt0
-.long _bus_error
-.long _address_error
-.long _illegal
-.long _zero_div
-.long _chk_inst
-.long _trapv
+.long 0xF0FFFFFC   /* Initial SP */
+.long crt0         /* Reset PC */
+VGS_VECTOR _bus_error
+VGS_VECTOR _address_error
+VGS_VECTOR _illegal, 1
+VGS_VECTOR _zero_div
+VGS_VECTOR _chk_inst
+VGS_VECTOR _trapv
+
+/* Fill remaining vectors with _illegal to catch unexpected traps. */
+VGS_VECTOR _illegal, (256 - 8)
