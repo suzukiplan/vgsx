637c9b741cc75b50cafd3aeb380c70f3b95ff7ff
suzukiplan <suzukiplan.ys@gmail.com>
2025-11-15 21:55:47 +0900

    bugfix: Correct SP (CRITICAL BUG)

--- src/vgsx.cpp
+++ src/vgsx.cpp
@@ -585,8 +585,12 @@ void VGSX::reset(void)
     if (this->ignoreReset) {
         return;
     }
+    constexpr uint32_t RAM_BASE = 0xF00000;
+    const uint32_t ramSize = static_cast<uint32_t>(sizeof(this->ctx.ram));
+    const uint64_t ramLimit = static_cast<uint64_t>(RAM_BASE) + ramSize;
+
     m68k_pulse_reset();
-    m68k_set_reg(M68K_REG_SP, 0);
+    m68k_set_reg(M68K_REG_SP, RAM_BASE + ramSize - 4);
     this->detectReferVSync = false;
     this->exitFlag = false;
     this->exitCode = 0;
@@ -610,10 +614,6 @@ void VGSX::reset(void)
     loadElfHeader(&eh, this->ctx.elf);
     putlog(LogLevel::I, "M68K_REG_PC = 0x%06X", eh.e_entry);
     m68k_set_reg(M68K_REG_PC, eh.e_entry);
-
-    constexpr uint32_t RAM_BASE = 0xF00000;
-    const uint32_t ramSize = static_cast<uint32_t>(sizeof(this->ctx.ram));
-    const uint64_t ramLimit = static_cast<uint64_t>(RAM_BASE) + ramSize;
 
     // Reset RAM
     memset(this->ctx.ram, 0xFF, sizeof(this->ctx.ram));
