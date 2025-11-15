82645ce6b970d6297ea24b03cadb0e26510a452e
suzukiplan <suzukiplan.ys@gmail.com>
2025-11-16 08:03:13 +0900

    correct RAM address

--- lib/vectors.s
+++ lib/vectors.s
@@ -1,6 +1,10 @@
 .section .vectors,"ax"
 .global __vgs_vectors
 
+.set RAM_BASE, 0x00F00000
+.set RAM_SIZE, 0x00100000
+.set RAM_TOP, RAM_BASE + RAM_SIZE
+
 .macro VGS_VECTOR handler, count=1
 .rept \count
 .long \handler
@@ -8,7 +12,7 @@
 .endm
 
 __vgs_vectors:
-.long 0xF0FFFFFC   /* Initial SP */
+.long RAM_TOP - 4   /* Initial SP (top of WRAM) */
 .long crt0         /* Reset PC */
 VGS_VECTOR _bus_error
 VGS_VECTOR _address_error
